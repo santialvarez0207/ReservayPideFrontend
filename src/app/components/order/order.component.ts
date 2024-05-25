9aaafa8ef6ed72fe9487e379c6e8cb7e03de65dd
santialvarez0207 <68878932+santialvarez0207@users.noreply.github.com>
2024-05-24 20:46:01 -0500

    vista de orden y modal de compra

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrl: './order.component.css'
 })
 export class OrderComponent {
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {
+  }
+  ngOnInit() {
+    this.addLinkElement();
+    /*this.loadScript('assets/xato/plugins/apex/apexcharts.min.js');
+    this.loadScript('assets/xato/plugins/flatpickr/flatpickr.js');
+    this.loadScript('assets/js/dashboard.js');*/
+  }
 
+  addLinkElement() {
+
+    const link3: HTMLLinkElement = this.renderer.createElement('link');
+    this.renderer.setAttribute(link3, 'rel', 'stylesheet');
+    this.renderer.setAttribute(link3, 'href', 'assets/Xato/assets/css/apps/ecommerce.css');
+    this.renderer.appendChild(this.document.head, link3);
+
+  }
 }
